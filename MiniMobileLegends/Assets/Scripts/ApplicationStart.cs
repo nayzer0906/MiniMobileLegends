69df7d76a3e848e5d8de6c211ebda3955bad8141
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-10-11 00:15:01 +0600

    Turrets spawning

--- MiniMobileLegends/Assets/Scripts/ApplicationStart.cs
+++ MiniMobileLegends/Assets/Scripts/ApplicationStart.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 public class ApplicationStart : MonoBehaviour
 {
     [SerializeField] private Transform[] environment = new Transform[3];
+    [SerializeField] private Transform[] turrets;
     private void Awake()
     {
         var map = Instantiate(environment[0], transform.position, Quaternion.identity);
         var castleAlly = Instantiate(environment[1], new Vector3(-63f, 0f, -63f), Quaternion.identity);
         var castleEnemy = Instantiate(environment[2], new Vector3(63f, 0f, 63f), Quaternion.identity);
+
+        foreach (var turret in turrets)
+        {
+            Instantiate(turret, turret.transform.position, turret.rotation);
+        }
     }
 
     void Start()
