a9a1ae05bd633bb0a464cb00f7d7fe96daf84939
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-10-10 22:26:44 +0600

    Map and castles spawning

---
+++ MiniMobileLegends/Assets/Scripts/ApplicationStart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationStart : MonoBehaviour
+{
+    [SerializeField] private Transform[] environment = new Transform[3];
+    private void Awake()
+    {
+        var map = Instantiate(environment[0], transform.position, Quaternion.identity);
+        var castleAlly = Instantiate(environment[1], new Vector3(-63f, 0f, -63f), Quaternion.identity);
+        var castleEnemy = Instantiate(environment[2], new Vector3(63f, 0f, 63f), Quaternion.identity);
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
