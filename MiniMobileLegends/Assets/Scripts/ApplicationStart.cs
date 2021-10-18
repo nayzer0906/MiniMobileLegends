35bf5362ccd4d5f56deb5132297f4012e64124ca
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-10-19 00:45:23 +0600

    Turtle monster spawning

--- MiniMobileLegends/Assets/Scripts/ApplicationStart.cs
+++ MiniMobileLegends/Assets/Scripts/ApplicationStart.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 
 public class ApplicationStart : MonoBehaviour
 {
+    [SerializeField] private Transform turtle_monster;
     void Start()
     {
-        
+        StartCoroutine(MonsterSpawn());
     }
-    
+
+    IEnumerator MonsterSpawn()
+    {
+        yield return new WaitForSeconds(10f);
+        Debug.Log("10 seconds passed...");
+        Instantiate(turtle_monster, new Vector3(40f, turtle_monster.position.y, 40f), Quaternion.identity);
+    }
+
     void Update()
     {
         
