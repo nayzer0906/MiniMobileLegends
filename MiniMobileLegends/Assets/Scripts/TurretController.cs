09f281ddd4fa8efd9c3cb688112033e8ffe2a079
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-10-22 02:25:08 +0600

    Particle system for turrets and castles is added

---
+++ MiniMobileLegends/Assets/Scripts/TurretController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ShootIntruders(other);
+    }
+
+    private void ShootIntruders(Collider intruder)
+    {
+        Destroy(intruder.gameObject);
+    }
+}
