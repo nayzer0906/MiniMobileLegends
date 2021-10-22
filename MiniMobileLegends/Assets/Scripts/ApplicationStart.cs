39b97a5a167f323a28d58f35c70979708b6b0fde
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-10-22 20:32:44 +0600

    Monsters' spawn is fixed

--- MiniMobileLegends/Assets/Scripts/ApplicationStart.cs
+++ MiniMobileLegends/Assets/Scripts/ApplicationStart.cs
@@ -23,32 +23,32 @@ public class ApplicationStart : MonoBehaviour
     {
         yield return new WaitForSeconds(15f);
         Debug.Log("15 seconds passed...");
-        Instantiate(monster_turtle, new Vector3(40f, monster_turtle.position.y, 40f), Quaternion.identity);
-        Instantiate(monster_turtle, new Vector3(-40f, monster_turtle.position.y, -40f), Quaternion.identity);
+        Instantiate(monster_turtle, new Vector3(-40f, monster_turtle.position.y, 40f), Quaternion.identity);
+        Instantiate(monster_turtle, new Vector3(40f, monster_turtle.position.y, -40f), Quaternion.identity);
     }
     
     IEnumerator LizardSpawn()
     {
         yield return new WaitForSeconds(10f);
         Debug.Log("10 seconds passed...");
-        Instantiate(monster_lizard, new Vector3(15f, monster_lizard.position.y, -55f), Quaternion.identity);
-        Instantiate(monster_lizard, new Vector3(-15f, monster_lizard.position.y, 55f), Quaternion.identity);
+        Instantiate(monster_lizard, new Vector3(-5f, monster_lizard.position.y, 55f), Quaternion.identity);
+        Instantiate(monster_lizard, new Vector3(5f, monster_lizard.position.y, -55f), Quaternion.identity);
     }
     
     IEnumerator CrabSpawn()
     {
         yield return new WaitForSeconds(6f);
         Debug.Log("6 seconds passed...");
-        Instantiate(monster_crab, new Vector3(15f, monster_crab.position.y, 15f), Quaternion.identity);
-        Instantiate(monster_crab, new Vector3(-15f, monster_crab.position.y, -15f), Quaternion.identity);
+        Instantiate(monster_crab, new Vector3(-15f, monster_crab.position.y, 15f), Quaternion.identity);
+        Instantiate(monster_crab, new Vector3(15f, monster_crab.position.y, -15f), Quaternion.identity);
     }
     
     IEnumerator SlimeSpawn()
     {
         yield return new WaitForSeconds(8f);
         Debug.Log("8 seconds passed...");
-        Instantiate(monster_slime, new Vector3(55f, monster_slime.position.y, 5f), Quaternion.identity);
-        Instantiate(monster_slime, new Vector3(-55f, monster_slime.position.y, -5f), Quaternion.identity);
+        Instantiate(monster_slime, new Vector3(-55f, monster_slime.position.y, -15f), Quaternion.identity);
+        Instantiate(monster_slime, new Vector3(55f, monster_slime.position.y, 15f), Quaternion.identity);
     }
 
     void Update()
