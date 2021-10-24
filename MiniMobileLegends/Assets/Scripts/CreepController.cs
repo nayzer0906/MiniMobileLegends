3050464dbc85117862ff51927fb20c7b939d45e0
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-10-24 22:44:16 +0600

    Creeps moving forward

---
+++ MiniMobileLegends/Assets/Scripts/CreepController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreepController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
