c9daf882434ada697d33c4b43b842bfa5012f850
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-11-07 02:41:59 +0600

    Splash screen is added, main menu is updated

---
+++ MiniMobileLegends/Assets/_Scripts/RotateMap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMap : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(0, 5f * Time.deltaTime, 0, Space.Self);
+    }
+}
