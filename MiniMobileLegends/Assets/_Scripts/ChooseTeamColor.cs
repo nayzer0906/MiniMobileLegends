c217e9ac96e0a967e2b3dfcf612780a54beeecfc
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-11-07 23:54:15 +0600

    Choose team color OnClick events

---
+++ MiniMobileLegends/Assets/_Scripts/ChooseTeamColor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseTeamColor : MonoBehaviour
+{
+    private SkinnedMeshRenderer hoodieColor;
+
+    private void OnEnable()
+    {
+        hoodieColor = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    private void Awake()
+    {
+        
+    }
+}
