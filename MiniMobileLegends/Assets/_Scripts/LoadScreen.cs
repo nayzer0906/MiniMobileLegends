d01eb423841003f12126c20e3f28041505cb3028
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-11-07 17:17:12 +0600

    Load screen

---
+++ MiniMobileLegends/Assets/_Scripts/LoadScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadScreen : MonoBehaviour
+{
+    private Slider slider;
+    private void OnEnable()
+    {
+        slider = GetComponent<Slider>();
+        StartCoroutine(ProgressBar());
+    }
+    
+    IEnumerator ProgressBar()
+    {
+        while (slider.value < slider.maxValue)
+        {
+            slider.value += 0.2f;
+            yield return new WaitForSeconds(0.3f);
+            if (slider.value >= slider.maxValue)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
