c9f66dff7dd2fed009ab0f433d88e67e2b5b983c
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-11-09 04:28:41 +0600

    Adjustments

--- MiniMobileLegends/Assets/_Scripts/EnemyChoosesColor.cs
+++ MiniMobileLegends/Assets/_Scripts/EnemyChoosesColor.cs
@@ -24,7 +24,7 @@ public class EnemyChoosesColor : MonoBehaviour
     public GameObject enemyColorChoosePnl;
     public GameObject map;
 
-    private void Start()
+    private void OnEnable()
     {
         EnemyChooses();
     }
